9618477fab6d31ab3a758e20380ce1d8096addcd
Q-N-LMY <69880934+Q-N-LMY@users.noreply.github.com>
2020-08-19 16:29:58 +0800

    Update GIT.java

--- java11/src/java11/GIT.java
+++ java11/src/java11/GIT.java
@@ -8,6 +8,7 @@ public static void main(String[] args) {
 	System.out.println("lmy123456789");
 	System.out.println("lmy0123456789");
 	System.out.println("qnlmy0123456789");
+	System.out.println("qnlmy01234567890");
 }
 
 }
